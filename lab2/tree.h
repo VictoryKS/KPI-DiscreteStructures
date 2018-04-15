e3a55b9ae620455ae8855b0c2875e780ec2ec6bb
VictoryKS <victory.kosholap@gmail.com>
2018-04-15 13:53:28 +0300

    Code refactoring

--- lab2/tree.h
+++ lab2/tree.h
@@ -21,18 +21,18 @@ class tree {
     tree::Node *_search_node(double data); // deletition's and contains' helper
     void _comparing(Node *deleted, Node *side); // deletition's helper
     void delete_node(double data);
-    double average();
+    double average(); // find average of all elements
     void add_average() {
-      double average = this->average();
+      double average = this->average(); // add average to the tree
       this->add(average);
     };
-    int get_count() {
+    int get_count() { // count of all elements
       return count;
     }
-    double get_sum() {
+    double get_sum() { // sum of all elements
       return sum;
     }
-    bool contains(double data) {
+    bool contains(double data) { // returns true if element contains in the tree
       return !!this->_search_node(data);
     }
 };
